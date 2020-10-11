d793bcf94f60746363bd96b2b2f07959234e68dd
maya <maya.marton@firstdag.com>
2020-10-11 18:41:18 +0300

    payment_service_test.py pass

--- vasp/backend/test/merchant_vasp/payment_service_test.py
+++ vasp/backend/test/merchant_vasp/payment_service_test.py
@@ -1,5 +1,8 @@
 import pytest
 
+from merchant_vasp import payment_service
+from merchant_vasp.onchainwallet import OnchainWallet
+from merchant_vasp.storage import Payment, PaymentStatus
 from ..webapp.conftest import (
     REJECTED_PAYMENT_SUBADDR,
     SENDER_MOCK_ADDR,
@@ -8,40 +11,35 @@ from ..webapp.conftest import (
     PAYMENT_SUBADDR,
     PAYMENT_AMOUNT,
     PAYMENT_CURRENCY,
-    PAYMENT_TX_ID,
     PAYMENT_ID,
     db,
 )
-from merchant_vasp import payment_service
-from merchant_vasp.storage import Payment, PaymentStatus
-from merchant_vasp.onchainwallet import OnchainWallet
 
 
 def test_payment_to_wrong_address(db):
     with pytest.raises(
-        payment_service.WrongReceiverAddressException, match="wrongaddr"
+            payment_service.WrongReceiverAddressException, match="wrongaddr"
     ) as e:
         payment_service.process_incoming_transaction(
             version=0,
             sender_address=SENDER_MOCK_ADDR,
-            sender_subaddress=SENDER_MOCK_SUBADDR,
+            sender_sub_address=SENDER_MOCK_SUBADDR,
             receiver_address="cccccccccccccccccccccccccccccccc",  # wrong addr
-            receiver_subaddress="ffffffffffffffff",
+            receiver_sub_address="ffffffffffffffff",
             amount=100,
             currency="LBR",
-            sequence=349,
         )
 
 
-def test_payment_to_wrong_subaddress(db):
+def test_payment_to_wrong_sub_address(db):
     with pytest.raises(
-        payment_service.PaymentForSubaddrNotFoundException, match="wrongsubaddr"
+            payment_service.PaymentForSubaddrNotFoundException, match="wrongsubaddr"
     ) as e:
         payment_service.process_incoming_transaction(
             sender_address=SENDER_MOCK_ADDR,
             sender_sub_address=SENDER_MOCK_SUBADDR,
-            receiver_address=OnchainWallet().vasp_address,
-            receiver_sub_address="ffffffffffffffff", # wrong sub address
+            receiver_address=OnchainWallet().address_str,
+            receiver_sub_address="ffffffffffffffff",  # wrong sub address
             amount=100,
             currency="LBR",
             version=0,
@@ -50,12 +48,12 @@ def test_payment_to_wrong_subaddress(db):
 
 def test_payment_status_invalid(db):
     with pytest.raises(
-        payment_service.PaymentStatusException, match="invalidpaymentstatus"
+            payment_service.PaymentStatusException, match="invalidpaymentstatus"
     ) as e:
         payment_service.process_incoming_transaction(
             sender_address=SENDER_MOCK_ADDR,
             sender_sub_address=SENDER_MOCK_SUBADDR,
-            receiver_address=OnchainWallet().vasp_address,
+            receiver_address=OnchainWallet().address_str,
             receiver_sub_address=REJECTED_PAYMENT_SUBADDR,
             amount=100,
             currency="LBR",
@@ -65,12 +63,12 @@ def test_payment_status_invalid(db):
 
 def test_payment_expired(db):
     with pytest.raises(
-        payment_service.PaymentExpiredException, match="paymentexpired"
+            payment_service.PaymentExpiredException, match="paymentexpired"
     ) as e:
         payment_service.process_incoming_transaction(
             sender_address=SENDER_MOCK_ADDR,
             sender_sub_address=SENDER_MOCK_SUBADDR,
-            receiver_address=OnchainWallet().vasp_address,
+            receiver_address=OnchainWallet().address_str,
             receiver_sub_address=EXPIRED_PAYMENT_SUBADDR,
             amount=100,
             currency="LBR",
@@ -80,12 +78,12 @@ def test_payment_expired(db):
 
 def test_invalid_payment_amount(db):
     with pytest.raises(
-        payment_service.PaymentOptionNotFoundException, match="paymentoptionnotfound"
+            payment_service.PaymentOptionNotFoundException, match="paymentoptionnotfound"
     ) as e:
         payment_service.process_incoming_transaction(
             sender_address=SENDER_MOCK_ADDR,
             sender_sub_address=SENDER_MOCK_SUBADDR,
-            receiver_address=OnchainWallet().vasp_address,
+            receiver_address=OnchainWallet().address_str,
             receiver_sub_address=PAYMENT_SUBADDR,
             amount=PAYMENT_AMOUNT - 1,  # underpaying
             currency=PAYMENT_CURRENCY,
@@ -93,12 +91,12 @@ def test_invalid_payment_amount(db):
         )
 
     with pytest.raises(
-        payment_service.PaymentOptionNotFoundException, match="paymentoptionnotfound"
+            payment_service.PaymentOptionNotFoundException, match="paymentoptionnotfound"
     ) as e:
         payment_service.process_incoming_transaction(
             sender_address=SENDER_MOCK_ADDR,
             sender_sub_address=SENDER_MOCK_SUBADDR,
-            receiver_address=OnchainWallet().vasp_address,
+            receiver_address=OnchainWallet().address_str,
             receiver_sub_address=PAYMENT_SUBADDR,
             amount=PAYMENT_AMOUNT + 1,  # overpaying
             currency=PAYMENT_CURRENCY,
@@ -108,12 +106,12 @@ def test_invalid_payment_amount(db):
 
 def test_invalid_payment_currency(db):
     with pytest.raises(
-        payment_service.PaymentOptionNotFoundException, match="paymentoptionnotfound"
+            payment_service.PaymentOptionNotFoundException, match="paymentoptionnotfound"
     ) as e:
         payment_service.process_incoming_transaction(
             sender_address=SENDER_MOCK_ADDR,
             sender_sub_address=SENDER_MOCK_SUBADDR,
-            receiver_address=OnchainWallet().vasp_address,
+            receiver_address=OnchainWallet().address_str,
             receiver_sub_address=PAYMENT_SUBADDR,
             amount=PAYMENT_AMOUNT,
             currency="USD",
@@ -126,7 +124,7 @@ def test_successful_payment(db):
     payment_service.process_incoming_transaction(
         sender_address=SENDER_MOCK_ADDR,
         sender_sub_address=SENDER_MOCK_SUBADDR,
-        receiver_address=OnchainWallet().vasp_address,
+        receiver_address=OnchainWallet().address_str,
         receiver_sub_address=PAYMENT_SUBADDR,
         amount=PAYMENT_AMOUNT,
         currency=PAYMENT_CURRENCY,
