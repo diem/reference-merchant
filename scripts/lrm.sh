7dec4ca6eae2cf664f8e4372da8b969b4334aa67
maya <maya.marton@firstdag.com>
2020-12-09 17:48:48 +0200

    fix lrm.sh

--- scripts/lrm.sh
+++ scripts/lrm.sh
@@ -162,7 +162,7 @@ setup_environment() {
   sh -c "cd liquidity && pipenv install --dev"
 
   info "***Setup Liquidity Provider***"
-  pipenv run python liquidity/setup_env.py || fail "Fail execute setup_env.py"
+  PIPENV_PIPFILE=liquidity/Pipfile pipenv run python liquidity/setup_env.py || fail "Fail execute setup_env.py"
 
   info "***Setting up environment .env files***"
   PIPENV_PIPFILE=vasp/backend/Pipfile pipenv run python scripts/set_env.py || fail "Fail execute scripts/set_env.py"
